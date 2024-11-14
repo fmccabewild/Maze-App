abe932d1d2c53ca3093acb98e9454f66de57d32f
fmccabewild <fmccabewild@stu.naperville203.org>
2024-11-14 09:48:23 -0600

    maze

--- src/Maze.java
+++ src/Maze.java
@@ -10,18 +10,54 @@ public class Maze
 
     public boolean loadMaze(String fname)
     {   
-        file = 
+        file = new Scanner(fname);
+        int numRows = file.readNext();
+        int numCols = file.readNext();
+        
         this.maze = new Square[numRows][numCols];
-
         for (int row = 0; row < numRows; row++)
         {
             for (int col = 0; col < numCols; col++)
-                maze[row][col] = null;
+                maze[row][col] = file.readNext();
         }
     }
 
     public ArrayList<Square> getNeighbors(Square sq)
     {
-        
+        ArrayList<Square> neighbors = new ArrayList<>();
+        neighbors.add(maze[sq.getRow - 1][sq.getCol]);
+        neighbors.add(maze[sq.getRow + 1][sq.getCol]);
+        neighbors.add(maze[sq.getRow][sq.getCol - 1]);
+        neighbors.add(maze[sq.getRow][sq.getCol + 1]);
+        for(Square neighbor: neighbors)
+        {
+            if(neighbor == null)
+                neighbors.remove(neighbor);
+        }
+        return neighbors;
+    }
+
+    public Square getStart()
+    {
+        for(Square [] row: maze)
+        {
+            for(Square square: row)
+            {
+                if(square.getType() == 2)
+                    return square;
+            }
+        }
+    }
+
+    public Square getFinish()
+    {
+        for(Square [] row: maze)
+        {
+            for(Square square: row)
+            {
+                if(square.getType() == 3)
+                    return square;
+            }
+        }
     }
 }
